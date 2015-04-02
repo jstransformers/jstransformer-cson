a5d24da95cff772cecbf09fc93faf6471570ab96
Timothy Gu <timothygu99@gmail.com>
2015-04-01 18:08:40 -0700

    Pretty print test

--- test/index.js
+++ test/index.js
@@ -4,9 +4,16 @@ var assert = require('assert');
 
 var transform = require('../');
 
-var res = transform.render('{\n    # an array\n    ' +
-                            'abc: [\n        \'a\'\n' +
-                            '        \'b\'\n        \'c\'\n    ]\n' +
-                            '    # an object\n    a:\n        b: \'c\'\n}');
+var res = transform.render('{'               +
+                         '\n    # an array'  +
+                         '\n    abc: ['      +
+                         "\n        'a'"     +
+                         "\n        'b'"     +
+                         "\n        'c'"     +
+                         '\n    ]'           +
+                         '\n    # an object' +
+                         '\n    a:'          +
+                         "\n        b: 'c'"  +
+                         '\n}');
 assert.deepEqual(JSON.parse(res), {"abc":["a","b","c"],"a":{"b":"c"}});
 console.log('test passed');
